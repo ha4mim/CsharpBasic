db9627a376897126242545e4d565ff948e0ef332
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2025-07-28 08:33:20 +0600

    Committing First Program

---
+++ 01.FirstProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class FirstProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(123);
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+}
