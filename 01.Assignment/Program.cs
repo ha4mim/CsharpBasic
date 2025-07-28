427ab17f67ace944425a293890020a13a18b7a25
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2025-07-28 08:44:33 +0600

    commiting Assignment

---
+++ 01.Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Assignment
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("My name is Sheikh Hamim Mahmud");
+        Console.WriteLine("I am 27 years old");
+        Console.WriteLine("I am a Software Engineer");
+    }
+}
